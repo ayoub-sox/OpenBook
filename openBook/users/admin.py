6a5d1200673338b0588dc30419844f114e5c89d9
Ayoub <ayoubbelghoul90@gmail.com>
2024-07-17 10:18:51 -0700

    Not a clean code

---
+++ openBook/users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import (
+    CustomUser,
+    Post,
+    Comment,
+    Topic,
+    Like,
+    Profile,
+    Follow,
+    Share,
+)
+
+# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Topic)
+admin.site.register(Like)
+admin.site.register(Profile)
+admin.site.register(Follow)
+admin.site.register(Share)
